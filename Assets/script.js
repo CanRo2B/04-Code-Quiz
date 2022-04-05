bca6997259f8c92b93f673813a2e97956625a5fe
Candace Robbins <canrob0522@gmail.com>
2022-04-04 23:04:31 -0500

    Completely revamping everything

--- Assets/script.js
+++ Assets/script.js
@@ -1,40 +1,3 @@
-// var timeElement = document.querySelector("#start").addEventListener("click", function() {
-
-// // var mainElement = document.getElementById(".countdown");
-
-// var timeLeft= 30;
-
-    
-// // Set interval to count down every 1000 milliseconds.
-// var timeInterval = setInterval(function function1(){ 
-//     document.querySelector(".countdown").innerHTML= timeLeft + " " + "seconds remaining";
-//     timeLeft--;
-//     timeElement.textContent = timeLeft + " seconds remaining";
-
-//     timeLeft -=1;
-//     if (timeLeft <= 0) {
-//         clearInterval(timeInterval);
-//         document.querySelector(".countdown").innerHTML = "Game over"
-//     }
-// }, 1000);
-
-// });
-
-    // if (timeLeft> 1) {
-    //     mainElement.textContent = timeLeft + " seconds remaining";
-    //     timeLeft--;
-    // } else if (timeLeft ===1) {
-    //         timeElement.textContent = timeLeft + " second remaining";
-    //         timeLeft--;
-    // } else {
-    //     timeElement.textContent = "Game over ";
-    //     clearInterval(timeInterval);
-    
-
-
-
-
-
 
 var questionInput = document.querySelector(".Question");
 var  AnswerOptionsInput = document.querySelector(".Answer-options");
@@ -42,18 +5,24 @@ var win = document.querySelector(".win");
 var lose = document.querySelector(".lose");
 var startButton= document.getElementById("start");
 var timeElement= document.querySelector(".countdown");
+var options=document.querySelector("#options");
+console.log(options);
 
 var question= " ";
 var correctAnswer= " ";
 var winCount= 0;
 var loseCount= 0;
 var isWin = false;
-var timeLeft;
+var timeLeft=30;
 var countdown;
 
 // Arrays used for the questions and answers.
+var questionIndex=0;
 var askQuestions= [];
 var answer= [];
+
+
+startButton.onclick= startGame;
 
 // Array of questions and possible answers.
 const questions =[
@@ -65,13 +34,44 @@ const questions =[
     answer: ["an in-vest-igator", "a goose.", "lunch for other alligators.", "an in-vest-ment"], correctAnswer: "an in-vest-igator"},
     {askQuestions: "Two peanuts walk into a bar...",
     answer: [ "they were eaten.", "they were crushed.", "one was a-salted", "the bar was called nuts"], correctAnswer:"one was a-salted"},
+    {askQuestions: "I used to be a banker...",
+    answer: [
+     "then I lost all my money.", "then I was robbed.", "and then I lost interest.",],
+    correctAnswer: "and then I lost interest."
+    },
+    {
+    askQuestions: "I was addicted to the hokey-pokey...",
+    answer: [ "but then I turned myself around.", "but then I went to Hokey Anonymous.", "but then I put my right foot in.",
+    ],
+        correctAnswer: "but then I turned myself around."
+    },
+    {
+    askQuestions: "I tried to come up with a carpentry pun...",
+    answer: [ "I nailed it.", "but I could not hit it on the head.", "that wood-work.",
+    ],
+        correctAnswer: "that wood-work."
+    },
+    {
+    askQuestions: "I think I nailed it but...",
+    answer: ["I missed it on the head.","nobody saw it.", "I was wrong.",
+    ],
+    correctAnswer: "nobody saw it."
+    },
+    {askQuestions: "If you keep holding your pee..",
+    answer: ["you might pee your pants",
+    "tinkle, tinkle little star.",
+     "urine trouble.",
+    ],
+    correctAnswer: "urine trouble."
+    },
+    {askQuestions: "What do you call a grumpy cow?",
+    answer: ["moo-dy!", "ground meat!", "moo-thy!",], 
+    correctAnswer: "moo-dy!",
+    },
     ]
 
-    let askQuestionsObj= questions.splice(Math.floor(Math.random() * questions.length), 1);
-
-    console.log(askQuestionsObj.answer);
-
-
+    
+// Initiate wins and losses
 function init(){
     getWins();
     getlosses();
@@ -79,10 +79,9 @@ function init(){
 
 // Start game when start button is pushed.  
 function startGame() {
-    isWin=false;
-    timeLeft=30;
+
     // Start button will not be able to be pushed until game is finished.
-    startButton.disabled = true;
+    // startButton.disabled = true;
 
     renderQuestions ()
     startTimer()
@@ -98,7 +97,7 @@ setWins()
 
 // When answered incorrectly the loseGame function is called.
 function loseGame() {
-correctAnswer.textContent= "Game over";
+correctAnswer.textContent= "Incorrect";
 loseCounter ++
 startButton.disabled = false;
 setLosses()
@@ -106,72 +105,111 @@ setLosses()
 
 function startTimer() {
 // Start Timer
-timeLeft= setInterval (function(){
-    countdown--;
-    timeLeftElement.textContent = countdown;
-    if (countdown >= 0){
-        // If answered correctly
-        if (isWin && countdown > 0){
-            // stops the timer
-            clearInterval(timeLeft);
-            winGame();
-        }
+countdown= setInterval (function(){
+    console.log(timeLeft)
+    timeLeft--;
+    timeElement.textContent = timeLeft + "seconds remaining";
+    if (timeLeft <= 0){
+        
+      else ;
+      if {setLosses(countdown = -5)
+        };
     }
     // If time runs out before answered correctly.
-    if (countdown ===0) {
+    if (timeLeft === 0) {
         clearInterval(timeLeft);
-        loseGame();
+       
     }
 }, 1000);
-}
+};
+// if (timeLeft> 1) {
+//         timeElement.textContent = timeLeft + " seconds remaining";
+//         timeLeft--;
+//     } else if (timeLeft ===1) {
+//             countdown.textContent = timeLeft + " second remaining";
+            
+//     } else {
+//         timeLeft=0;
+//         timeElement.textContent = "Game over ";
+//         clearInterval(timeInterval);
+//     }
 
+// Start the Game
 function renderQuestions(){
     // Randomly pick questions from the const questions
+    
+    let currentQuestion= questions[questionIndex]
+    questionInput.textContent=currentQuestion.askQuestions;
+    currentQuestion.answer.forEach(function(choice,i){
+        var answerButton=document.createElement("button")
+        answerButton.setAttribute("value", choice)
+        answerButton.textContent= i + 1 + ". " + choice
+        options.appendChild(answerButton)
+    })
+   
+
+    // Loop to assort the correct answer within the answers.
+    for (var i=0; i <correctAnswer; i++){
+        answers.push(" ");
+    }
+    // questionInput.textContent=question.innerHTML("");
+    // AnswerOptionsInput.textContent= answers.innerHTML("");
+    
+function checkAnswers (correctAnswer) {
+    userAnswer = (answer.querySelector('input[name=correctAnswer'+ i +']:checked')||{}).value;
+
+    if(userAnswer===answer[i].correctAnswer){
+
+        isWin++;
+
+        correctAnswer[i].style.color = "blue";
+    }
+        else{
+            answer[i].style.color="red";
+            setLosses ++;
+        }
+
+    }
 
 }
-// let askQuestions = [
-//     {
-//         question: "Where do boats go if they think they are sick?",
-//         answer: [
-//             {optionA: "To the sea!", answer:false},
-//             {optionB: "To a mechanic!", answer:false},
-//             {optionC: "To a dock!", answer:true},
-//             {optionD: "To the gas station!", answer:true}
-//         ]
-//     },
-//     {
-//         question: "Who ever invented the knock-knock joke...",
-//         answer: [
-//             {optionA: "is not very smart.", answer:false},
-//             {optionB: "is the smartest person in the world.", answer:false},
-//             {optionC: "is a-door-able.", answer:false},
-//             {optionD: "should get a no-bel prize", answer:true},
-//         ]
-//     },
-//     {
-//         question: "What do you call an alligator in a vest?",
-//         answer: [
-//             {optionA: "an in-vest-igator", answer: true},
-//             {optionB: "a goose.", answer: false},
-//             {optionC: "lunch for other alligators.", answer:false},
-//             {optionD: "an in-vest-ment", answer:false},
-//         ]
-//     },
-//     {
-//         question: "Two peanuts walk into a bar...",
-//         answer: [
-//             {optionA: "they were eaten.", answer:false},
-//             {optionB: "they were crushed.", answer:false},
-//             {optionC: "one was a-salted", answer:true},
-//             {optoinC: "the bar was called nuts", answer:false}
-//         ]
-//     },
-   
-//         ]
-//     }
-// ]
 
+function setWins(){
+    win.textContent=winsCounter;
+    localStorage.setItem("winCount", winsCounter);
+}
 
+function setLosses(){
+    lose.textContent = loseCounter;
+    localStorage.setItem("loseCount", loseCounter)
+}
 
+function getWins(){
+    var storedWins=localStorage.getItem("winCount");
+    if (storedWins === null) {
+        winCounter = 0;}
+        else {
+            winCounter = storedWins;
+        }
+    }
 
+function getlosses () {
+    var storedLosses = localStorage.getItem("loseCount");
+    if (storedLosses === null) {
+        loseCounter = 0;
+    } else {
+        loseCounter= storedLosses;
+    }
+    lose.textContent = loseCounter;
+}
 
+function checkWin() {
+    if (question === answers.join("")); {
+        isWin=true;
+    }
+};
+
+function checkAnswers (correctAnswer) {
+
+}
+// console.log(askQuestions)
+// console.log(askQuestionsObj.answer);
--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # 04-Code-Quiz
-This Repository will create a Web API Code Quiz
+## How Well Do You Know Dad Jokes?
+
+## Description
+This Repository will create a Web API Code Quiz.  I attempted to make a web quiz about "Dad Jokes".  We tell them often in our house, do you?  The quiz has 10 questions with three possible answers.  The user can click on the answer the user believes to be correct.  It will then record whether their answer is correct.  If it is not correct, it will record as Incorrect and deduct 5 seconds from your time remaining.  The game is over when the time is over.  The amount of correct and incorrect answers will then be logged into the console.  
+
+## Table of Contents
+1. Title
+2. Questions
+3. Possible answer buttons once the game is initiated
+4. Next question button
+5. Start button
+6. Countdown timer
+7. Records Correct and incorrect
+
+##  How to Run the Project
+Once the page loads and you are ready for the "oh so bad they are funny" jokes, click on the Start button.  Read through the question and answers.  Select the answer you feel is correct.  Then select Next.  Game is over when time alloted has ran down to 0.  
+
+## Screenshot
+
+
+
+## Credits
+I had lots of help from my tutor as well as using the CSS template from the Unit 4 Web API's Activity 28 Mini-project.  
+
+## Links
+[Website] 
